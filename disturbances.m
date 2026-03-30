4091d01d1c101412b932a6e0f6ae79361d251d43
Pradeep-001-hub <koramutlapradeep@gmail.com>
2026-03-30 07:08:49 +0530

    Create disturbances.m

---
+++ disturbances.m
@@ -0,0 +1,34 @@
+clear; clc; close all;
+
+A = [-0.05  0.02  0     0;
+      0.01 -0.06  0.03  0;
+      0     0.02 -0.07  0.02;
+      0     0     0.01 -0.05];
+
+B = [0.1 0   0   0;
+     0   0.1 0   0;
+     0   0   0.1 0;
+     0   0   0   0.1];
+
+D = [0.5 0   0;
+     0   0.4 0;
+     0   0   0.3;
+     0   0   0.2];
+
+T_ref = [300;310;280;320];
+x0 = [290;305;275;315];
+tspan = [0 50];
+
+Q = diag([100 100 50 80]);
+R = diag([1 1 1 1]);
+K = lqr(A,B,Q,R);
+
+[t,x] = ode45(@(t,x) lqr_system_function(t,x,K,A,B,D,T_ref), tspan, x0);
+
+figure
+plot(t,x,'LineWidth',2)
+grid on
+xlabel('Time (s)')
+ylabel('Temperature (K)')
+title('LQR Controller with Disturbances')
+legend('Electronics','Battery','Radiator','Engine')
